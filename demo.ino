5daaa912c431c97c1536f6a1645645149ebb7b8d
Karel Krops <karelkrops@gmail.com>
2022-11-26 00:20:43 +0100

    demo version 1.4.1

--- demo.ino
+++ demo.ino
@@ -15,19 +15,48 @@
 KraMotory motor1 = KraMotory(0, PIN_M1A, PIN_M1B, PIN_ENC1A, PIN_ENC1B);
 KraMotory motor2 = KraMotory(1, PIN_M2A, PIN_M2B, PIN_ENC2A, PIN_ENC2B);
 
+void testPower()
+{
+    Serial.println("Test motor1 power is starting.");
+    motor1.setPower(100); // power -255 .. 255
+    delay(2000);
+    motor1.setSpeed(0);
+    Serial.println("Test motor2 power is starting.");
+    motor2.setSpeed(100); // power -255 .. 255
+    delay(2000);
+    motor2.setSpeed(0);
+    Serial.println("Test motors was stoped.");
+    delay(2000);
+}
+void testSpeed()
+{
+    Serial.println("Test motor1 speed is starting.");
+    motor1.setSpeed(500); // speed v mm/s
+    delay(2000);
+    motor1.setSpeed(0);
+    Serial.println("Test motor2 speed is starting.");
+    motor2.setSpeed(500); // speed v mm/s
+    delay(2000);
+    motor2.setSpeed(0);
+    Serial.println("Test motors was stoped.");
+    delay(2000);
+}
+
 void setup()
 {
 
     Serial.begin(115200);
-     delay(1000);
+    delay(1000);
     Serial.println("Test motory was start.");
 
     motor1.init();
     motor2.init();
+    delay(2000);
+    testPower();
+    testSpeed();
 }
 void loop()
 {
-
 }
 void vypis()
 {
